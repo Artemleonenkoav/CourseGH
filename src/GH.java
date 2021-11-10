0498c8b591165037744c0b6de0bfcdb6acec6b6a
Artemleonenkoav <leonenkoavfin@gmail.com>
2021-11-10 20:02:12 +0200

    Ad date

--- src/GH.java
+++ src/GH.java
@@ -1,7 +1,13 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class GH {
     public static void main(String[] args) {
-        System.out.println("bla bla ");
-        System.out.println("bla bla ");
+        for (int i = 0; i < 2; i++) {
+            System.out.println(i);
+            System.out.println(LocalDateTime.now());
+
+        }
 
     }
 }
